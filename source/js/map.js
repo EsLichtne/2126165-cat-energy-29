7a91b381e254fd35c7b391344b0088b0d97948a8
Ксения <easyghoul@gmail.com>
2023-11-23 09:50:10 +0700

    Заменяет кавычки на одинарные

--- source/js/map.js
+++ source/js/map.js
@@ -1,12 +1,12 @@
 let screenWidth = window.screen.width;
-let map = document.querySelector(".invite__map");
-let mapImage = document.querySelector(".invite__map-image");
+let map = document.querySelector('.invite__map');
+let mapImage = document.querySelector('.invite__map-image');
 let center;
 let zoom;
 let pinSize;
 
-map.classList.remove("invite__map--nojs");
-mapImage.classList.remove("invite__map-image--nojs");
+map.classList.remove('invite__map--nojs');
+mapImage.classList.remove('invite__map-image--nojs');
 
 if (screenWidth < 1280) {
   center = [59.938631, 30.323037];
@@ -23,25 +23,25 @@ if (screenWidth < 640) {
 }
 
 function init() {
-  let map = new ymaps.Map("map", {
+  let map = new ymaps.Map('map', {
     center: center,
     zoom: zoom
   });
 
   let placemark = new ymaps.Placemark([59.93871181590914, 30.3230150423278], {}, {
-    iconLayout: "default#image",
-    iconImageHref: "./img/pin.svg",
+    iconLayout: 'default#image',
+    iconImageHref: './img/pin.svg',
     iconImageSize: pinSize,
     iconImageOffset: [-19, -44]
   });
 
-  map.controls.remove("geolocationControl");
-  map.controls.remove("searchControl");
-  map.controls.remove("trafficControl");
-  map.controls.remove("typeSelector");
-  map.controls.remove("fullscreenControl");
-  map.controls.remove("zoomControl");
-  map.controls.remove("rulerControl");
+  map.controls.remove('geolocationControl');
+  map.controls.remove('searchControl');
+  map.controls.remove('trafficControl');
+  map.controls.remove('typeSelector');
+  map.controls.remove('fullscreenControl');
+  map.controls.remove('zoomControl');
+  map.controls.remove('rulerControl');
   map.geoObjects.add(placemark);
 };
 
